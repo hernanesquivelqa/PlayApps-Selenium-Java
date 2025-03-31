060ac96e97026e484c8fb47a3b5688b622d3e502
hernan <hernan97carp@hotmail.com>
2025-03-31 10:45:00 -0300

    Refactor PagePopUp to use @FindBy with PageFactory for improved readability and maintainability

--- src/test/java/pages/PagePopUp.java
+++ src/test/java/pages/PagePopUp.java
@@ -1,55 +1,68 @@
 package pages;
+
 import java.util.Set;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class PagePopUp {
-	private WebDriver driver;
-	private By buttonLogin = By.id("login");
-	private By buttonPopUpSubmit = By.cssSelector("#wrapper button");
-	private By infoText = By.id("info");
-	private String url = "https://qaplayground.dev/apps/popup/#";
-	private String buttonNotClicked ="Click to open pop-up";
-	private String buttonClicked = "Button Clicked";
+    private WebDriver driver;
+    
+    @FindBy(id = "login")
+    private WebElement buttonLogin;
 
-	public PagePopUp(WebDriver driver) {
-		this.driver = driver;
-	}
-	
-	public void clickButton() {
-		driver.findElement(buttonLogin).click();
-	}
+    @FindBy(css = "#wrapper button")
+    private WebElement buttonPopUpSubmit;
 
-	public String getUrl() {
-		return url;
-	}
-	
-	public void popUpClickSubmit() {
-	    // Guardar el identificador de la ventana principal
-	    String mainWindow = driver.getWindowHandle();
-	    // Esperar hasta que se abra una nueva ventana
-	    Set<String> allWindows = driver.getWindowHandles();
-	    for (String window : allWindows) {
-	        if (!window.equals(mainWindow)) {
-	            driver.switchTo().window(window); // Cambiar al pop-up
-	            break;
-	        }
-	    }
-	
-	    // Hacer clic en el botón dentro del pop-up
-	    driver.findElement(buttonPopUpSubmit).click();
-	
-	    // Volver a la ventana principal
-	    driver.switchTo().window(mainWindow);
-	}
-	public String getInfoText() {
-		return driver.findElement(infoText).getText();
-	}
+    @FindBy(id = "info")
+    private WebElement infoText;
+    
+    private String url = "https://qaplayground.dev/apps/popup/#";
+    private String buttonNotClicked = "Click to open pop-up";
+    private String buttonClicked = "Button Clicked";
 
-	public String getButtonClicked() {
-		return buttonClicked;
-	}
-	public String getButtonNotClicked() {
-		return buttonNotClicked;
-	}
+    // Constructor con inicialización de PageFactory
+    public PagePopUp(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+    
+    public void clickButton() {
+        buttonLogin.click(); 
+    }
 
+    public String getUrl() {
+        return url;
+    }
+    
+    public void popUpClickSubmit() {
+        // Guardar el identificador de la ventana principal
+        String mainWindow = driver.getWindowHandle();
+        // Esperar hasta que se abra una nueva ventana
+        Set<String> allWindows = driver.getWindowHandles();
+        for (String window : allWindows) {
+            if (!window.equals(mainWindow)) {
+                driver.switchTo().window(window); // Cambiar al pop-up
+                break;
+            }
+        }
+    
+      
+        buttonPopUpSubmit.click();
+        // Volver a la ventana principal
+        driver.switchTo().window(mainWindow);
+    }
+
+    public String getInfoText() {
+        return infoText.getText();
+        		}
+
+    public String getButtonClicked() {
+        return buttonClicked;
+    }
+
+    public String getButtonNotClicked() {
+        return buttonNotClicked;
+    }
 }
