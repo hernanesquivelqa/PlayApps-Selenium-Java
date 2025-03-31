a53fc5ca6ed445925e7cfa851cf2fb124e66226a
hernan <hernan97carp@hotmail.com>
2025-03-31 11:33:19 -0300

    Refactor:Add @FindBy annotation for progressBar element in PageShadowDom

--- src/test/java/app/ShadowDomTest.java
+++ src/test/java/app/ShadowDomTest.java
@@ -1,9 +1,7 @@
 package app;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import java.time.Duration;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
@@ -11,41 +9,25 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import base.TestBase;
+import pages.PageShadowDom;
 
 public class ShadowDomTest extends TestBase {
+	private PageShadowDom pageShadowDom;
+	
 	@BeforeEach
     public void  setUpPage() {
-		goToUrl("https://qaplayground.dev/apps/shadow-dom/");
+		pageShadowDom = new PageShadowDom(driver);
+		goToUrl(pageShadowDom.getUrl());
 		
 	}
-	
-
 	@Test
-	public void secondTestCase() {
-		 JavascriptExecutor js = (JavascriptExecutor) driver;
-
-	        // Seleccionar el Shadow Host (<progress-bar>)
-	        WebElement shadowHost = driver.findElement(By.tagName("progress-bar"));
-	        assertEquals("5", shadowHost.getAttribute("percent"));
-
-	        // Acceder al Shadow Root y obtener el botón dentro del Shadow DOM
-	        WebElement button = (WebElement) js.executeScript(
-	            "return arguments[0].shadowRoot.querySelector('.btn-green-outline');", 
-	            shadowHost
-	        );
-	        button.click();
-        // Espera explícita para que el atributo "percent" llegue a "95" (máximo 10 segundos)
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        wait.until(driver -> {
-       
-            String attribute = shadowHost.getAttribute("percent");
-            return "95".equals(attribute);
-        });
-
-        String attribute = shadowHost.getAttribute("percent");
-        assertEquals("95", attribute);
+	public void initalStateOfProgressBar() {
+    WebElement initalStateOfProgressBar =  pageShadowDom.progressBar();
+    assertEquals("5", initalStateOfProgressBar.getAttribute("percent"));
+    pageShadowDom.clickProgressBar();
+    WebElement finalStateOfProgressBar = pageShadowDom.waitForProgressBarToBeAlmostFull();
+    assertEquals("95", finalStateOfProgressBar.getAttribute("percent"));
 	}
 	
 }
---
+++ src/test/java/pages/PageShadowDom.java
@@ -0,0 +1,47 @@
+package pages;
+import java.time.Duration;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class PageShadowDom {
+	private WebDriver driver;
+	@FindBy(tagName = "progress-bar" )
+	private WebElement progressBar;
+	
+	private String url = "https://qaplayground.dev/apps/shadow-dom/";
+  public PageShadowDom(WebDriver driver) {
+	  this.driver = driver;
+	  PageFactory.initElements(driver, this);
+	  
+  }
+public String getUrl() {
+	return url;
+}
+public WebElement progressBar() {
+    WebElement shadowHost = progressBar;
+    return shadowHost;
+}
+public void clickProgressBar () {
+	 JavascriptExecutor js = (JavascriptExecutor) driver;
+	WebElement shadowHost = progressBar();
+    WebElement button = (WebElement) js.executeScript(
+        "return arguments[0].shadowRoot.querySelector('.btn-green-outline');", 
+        shadowHost
+    );
+    button.click();
+}
+public WebElement waitForProgressBarToBeAlmostFull() {
+    WebElement shadowHost = progressBar();
+    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    wait.until(driver -> {
+        String attribute = shadowHost.getAttribute("percent");
+        return "95".equals(attribute);
+    });
+    return shadowHost;
+}
+
+}
